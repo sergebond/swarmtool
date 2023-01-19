d3aba1f3b72c0a79ba7fac2d1b987b7aa0a9a725
serhiibondarchuk <s.bondarchuk.mail@gmail.com>
2023-01-19 11:31:48 +0100

    Second commit fixes

--- src/swarmtool.erl
+++ src/swarmtool.erl
@@ -3,7 +3,7 @@
 
 %% API
 -export([
-  login_by_userid_and_pwd/2,
+  login_by_username_and_pwd/2,
   login_by_jwe/1,
   login_by_userid_and_auth_token/2,
   login_by_auth_token_and_jwe_token/2,
@@ -14,7 +14,7 @@
 %% swarmtool:auth(#{<<"user_id">> => 318889366, <<"auth_token">> => <<"53763B9F3D9750C27352C88D5DF53D14">>}).
 %% swarmtool:auth(#{<<"jwe_token">> => JweToken, <<"auth_token">> => AuthToken}}).
 
-login_by_userid_and_pwd(UserId, Password) ->
+login_by_username_and_pwd(UserId, Password) ->
   auth(#{<<"username">> => UserId, <<"password">> => Password}).
 
 login_by_jwe(JweToken) ->
